43f3c82a1fae189a68700ddf35844dfb4dbff4d2
atulvidyarthi29 <atulvidyarthi29@gmail.com>
2020-12-03 21:34:30 +0530

    zuul api-gateway initiated

--- zuul-gateway/src/main/java/com/haven/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/haven/zuulgateway/ZuulGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.haven.zuulapigateway;
+package com.haven.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZuulApiGatewayApplication {
+public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+		SpringApplication.run(ZuulGatewayApplication.class, args);
 	}
 
 }
--- zuul-gateway/src/test/java/com/haven/zuulgateway/ZuulGatewayApplicationTests.java
+++ zuul-gateway/src/test/java/com/haven/zuulgateway/ZuulGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.haven.zuulapigateway;
+package com.haven.zuulgateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ZuulApiGatewayApplicationTests {
+class ZuulGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
