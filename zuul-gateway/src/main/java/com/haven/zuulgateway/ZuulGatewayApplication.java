5bbaa5779f98c60c8e2421399650f4b45424ccaf
atulvidyarthi29 <atulvidyarthi29@gmail.com>
2020-12-17 01:39:02 +0530

    cors

--- zuul-gateway/src/main/java/com/haven/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/haven/zuulgateway/ZuulGatewayApplication.java
@@ -4,14 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class ZuulGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins("*");
+            }
+        };
+    }
 
 }
